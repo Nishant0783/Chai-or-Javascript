b88a30c1d587fb98b5d834e22b6615ef52c075fc
Nishant Paliwal <paliwalnishant2003@gmail.com>
2024-04-16 14:39:42 +0530

    Strings. Lecture 11 till 16/04/24

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,16 @@
+// In any language strings are very powerfull
+
+// There are many ways to create a string in JS.
+
+// 1) Directly declaring string
+const str = "newString"
+
+// 2) Using new keyword
+const newStr = new String("String using new keyword")
+
+// In second syntax of declaring strings, an object of String class is created with value provided in parenthesis.
+// This syntax stores each character as a key-value pair, means the position of character will be key and the character is value itself.
+/*********************REMEMBER: The strings are stored as object not as array.****************/
+
+// We can have many methods on strings.
+
